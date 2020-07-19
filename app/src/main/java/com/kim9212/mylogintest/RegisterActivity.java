cf52ff7fabf1bbe41d40f74d67ed2c12b17ec75e
Jamong435 <sesang435@naver.com>
2020-07-19 22:23:12 +0900

    1

--- app/src/main/java/com/kim9212/mylogintest/RegisterActivity.java
+++ app/src/main/java/com/kim9212/mylogintest/RegisterActivity.java
@@ -2,7 +2,8 @@ package com.kim9212.mylogintest;
 
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,25 +14,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText et_id, et_pass, et_name, et_age;
+    private EditText et_id,  et_name, et_age;
     private Button btn_register;
+
+    TextInputEditText et_pass;
+    TextInputLayout inputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 액티비티 시작시 처음으로 실행되는 생명주기!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
+        inputLayout= findViewById(R.id.layout_et);
+        inputLayout.setCounterEnabled(true);
+        inputLayout.setCounterMaxLength(10);
+        inputLayout.setPasswordVisibilityToggleEnabled(true);
+
         // 아이디 값 찾아주기
         et_id = findViewById(R.id.et_id);
         et_pass = findViewById(R.id.et_pass);
         et_name = findViewById(R.id.et_name);
         et_age = findViewById(R.id.et_age);
+
+        et_pass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if( s.toString().contains("#") ){
+                    inputLayout.setError("특수문자 사용금지");
+                }else{
+                    inputLayout.setError(null);
+                }
+
+                if( s.length() ==0 ) inputLayout.clearFocus();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         // 회원가입 버튼 클릭 시 수행
         btn_register = findViewById(R.id.btn_register);
